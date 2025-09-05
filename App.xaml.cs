582a9246e95c4b71b9bc9cf8623693ff4c4547a9
RATCM <isakbjoershol@gmail.com>
2025-09-05 19:11:20 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Untitled_Food_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Untitled_Food_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
